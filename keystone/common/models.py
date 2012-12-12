0885c1d44ffdbf81f62ccb6346af55852e4a49a9
kwss <kwss@kent.ac.uk>
2012-12-12 14:20:55 +0000

    Added migrate script for mapping service and fixed some formatting

--- keystone/common/models.py
+++ keystone/common/models.py
@@ -128,13 +128,14 @@ class Role(Model):
     required_keys = ('id', 'name')
     optional_keys = tuple()
 
-class OrgAttributeSet(Model):
-	"""Organisational Attribute Set object
 
-	Required keys:
-		id
-		name
-	
-	"""
-	required_keys = ('id', 'name')	
-	optional_keys = ('description')
+class OrgAttributeSet(Model):
+    """Organisational Attribute Set object
+
+    Required keys:
+        id
+        name
+
+    """
+    required_keys = ('id', 'name')
+    optional_keys = ('description')
